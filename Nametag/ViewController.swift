1fc47bb783f5d70603fd43bc7b0afcac13feddc6
thompsonate <nate@natethompson.io>
2017-10-14 04:17:16 -0400

    face detection!

--- Nametag/ViewController.swift
+++ Nametag/ViewController.swift
@@ -9,25 +9,53 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
+
+extension UIImage {
+    func crop( percentageRect: CGRect) -> UIImage {
+        let pixelRect = CGRect(
+            x: percentageRect.origin.x * self.size.width,
+            y: percentageRect.origin.y * self.size.height,
+            width: percentageRect.width * self.size.height,
+            height: percentageRect.height * self.size.width)
+        
+        let imageRef = self.cgImage!.cropping(to: pixelRect)
+        let image = UIImage(cgImage: imageRef!, scale: self.scale, orientation: self.imageOrientation)
+        return image
+    }
+    
+    func rotate() -> UIImage {
+        let rotatedSize = CGSize(width: self.size.height, height: self.size.width)
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()!
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        bitmap.translateBy(x: rotatedSize.width / 2.0, y: rotatedSize.height / 2.0)
+        
+        //   // Rotate the image context
+        bitmap.rotate(by: CGFloat.pi / 2)
+        
+        // Now, draw the rotated/scaled image into the context
+        bitmap.scaleBy(x: 1.0, y: -1.0)
+        bitmap.draw(self.cgImage!, in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+}
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var image: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,6 +90,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let pixelBuffer = sceneView.session.currentFrame?.capturedImage else {return}
+        
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let temporaryContext = CIContext(options: nil)
+        let videoImage = temporaryContext.createCGImage(ciImage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer)))
+        
+        image = UIImage(cgImage: videoImage!)
+        image = image.rotate()
+        
+        let faceLandmarksRequest = VNDetectFaceRectanglesRequest(completionHandler: self.handleFaceFeatures)
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .right, options: [:])
+        do {
+            try requestHandler.perform([faceLandmarksRequest])
+        } catch {
+            print(error)
+        }
+    }
+
+    func handleFaceFeatures(request: VNRequest, errror: Error?) {
+        guard let observations = request.results as? [VNFaceObservation] else {
+            fatalError("unexpected result type!")
+        }
+        
+        print(observations)
+        
+        for face in observations {
+            image = image.crop(percentageRect: face.boundingBox)
+            print(image)
+        }
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
